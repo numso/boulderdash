39919d36775fbd52608d7cbf192b0c3478ab39dc
numso <dosmun@gmail.com>
2013-05-28 12:02:05 -0600

    fixed input

--- client/input.js
+++ client/input.js
@@ -1,1 +1,20 @@
-a
+var keys = {};
+
+$(document).keydown(function (e){
+  keys[e.keyCode] = true;
+});
+
+$(document).keyup(function (e){
+  keys[e.keyCode] = false;
+});
+
+exports.left  = function () { return keys[65] || keys[37]; };
+exports.right = function () { return keys[68] || keys[39]; };
+exports.up    = function () { return keys[87] || keys[38]; };
+exports.down  = function () { return keys[83] || keys[40]; };
+
+exports.resetKeys = function () {
+  for (var key in keys) {
+    keys[key] = false;
+  }
+}
